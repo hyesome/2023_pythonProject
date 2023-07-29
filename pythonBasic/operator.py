4db2a567e3936a3a5b1b78b3c09d022f9795ce53
hyesome <yuhssn@gamil.com>
2023-07-29 11:37:51 +0900

    논리연산(and, or) 학습

--- pythonBasic/operator.py
+++ pythonBasic/operator.py
@@ -48,3 +48,18 @@ print(isBiggerOrEqualThan)
 # print(isSmallerThan)
 # print(isSmallerOrEqualThan)
 
+andCondition = numA > 20 and numB > 3
+orCondition = numA > 20 or numB > 3
+
+print(andCondition)
+print(orCondition)
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
