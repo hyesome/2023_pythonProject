a21a75c8e5b7b69d0a671eb1c5cf4ec3e5bd8cca
hyesome <yuhssn@gamil.com>
2023-07-29 11:11:08 +0900

    기본 사칙연산 학습

---
+++ pythonBasic/operator.py
@@ -0,0 +1,19 @@
+plus = 5 + 4
+minus = 10 - 7
+multiply = 20 * 3
+division = 9 / 3
+
+print(plus)
+print(minus)
+print(multiply)
+print(division)
+
+# Error case
+# divisionByZero = 9/0
+
+r = 13 % 4
+n = 13 // 4
+
+print(r)
+print(n)
+
