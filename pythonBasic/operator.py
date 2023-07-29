ec29bbd1f2ca4f4ca5f8dc55c442077b6163059d
hyesome <yuhssn@gamil.com>
2023-07-29 11:33:43 +0900

    논리연산 학습

--- pythonBasic/operator.py
+++ pythonBasic/operator.py
@@ -1,3 +1,5 @@
+# 기본 사칙 연산
+
 plus = 5 + 4
 minus = 10 - 7
 multiply = 20 * 3
@@ -16,4 +18,33 @@ n = 13 // 4
 
 print(r)
 print(n)
+
+test = (13 ** 50) / 17
+print(test)
+
+# 논리 연산
+numA = 20
+numB = 4
+
+isEqual = numA == numB
+isNotEqual = 5 != 4
+
+print(isEqual)
+print(isNotEqual)
+
+isEqual = numA == 20
+
+print(isEqual)
+
+isBiggerThan = numA > 20
+isBiggerOrEqualThan = numA >= 20
+
+print(isBiggerThan)
+print(isBiggerOrEqualThan)
+
+# isSmallerThan =
+# isSmallerOrEqualThan =
+#
+# print(isSmallerThan)
+# print(isSmallerOrEqualThan)
 
