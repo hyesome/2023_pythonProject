9d6e7c26b4c6aad2172f9797863ed37d72fd2d35
hyesome <yuhssn@gamil.com>
2023-08-19 10:42:05 +0900

    if문 코드 개선

--- pythonBasic/ifStatement.py
+++ pythonBasic/ifStatement.py
@@ -16,7 +16,10 @@ score = float(input("이번 외국어 영역의 점수를 숫자만 입력해주
 #     print("E반으로 가세요.")
 
 
-if score >= 90 and score <= 100:
+if score > 100 or score < 0:
+    print("값을 이상하게 입력한것 같아요.")
+
+elif score >= 90:
     print("A반으로 가세요.")
 
 elif score >= 80:
@@ -28,8 +31,5 @@ elif score >= 70:
 elif score >= 60:
     print("D반으로 가세요.")
 
-elif score >= 0 and score < 60:
-    print("E반으로 가세요.")
-
 else:
-    print("값을 이상하게 입력한것 같아요.")
+    print("E반으로 가세요.")
