1179f17ffb0bc78e7fb2ef8f43f9f02e35ad38a5
hyesome <yuhssn@gamil.com>
2023-11-11 11:35:23 +0900

    2048 메뉴 및 파일 추가

--- pythonBasic/games/main.py
+++ pythonBasic/games/main.py
@@ -1,5 +1,6 @@
 from pythonBasic.games.updown import runUpDown
 from pythonBasic.games.hangman import runHangMan
+from pythonBasic.games.game2048 import run2048
 
 def menuPrint():
     print("=======GAME=======")
@@ -19,3 +20,5 @@ while userInput != 0:
         runHangMan()
     elif userInput == 2:
         runUpDown()
+    elif userInput == 3:
+        run2048()
